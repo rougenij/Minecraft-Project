36d284101f8db2ef994a837d094d59226a0acf20
Rouge Nij <rougenij@gmail.com>
2021-12-03 22:22:03 +0200

    Added SelectedTool and Mineable function

---
+++ scripts/tools.js
@@ -0,0 +1,26 @@
+const tools = [...document.querySelectorAll(".tools")];
+
+const toolsMaterial = {
+  pickaxe: ["stone"],
+  shovel: ["dirt", "grass"],
+  axe: ["wood", "leaves"],
+  none: [],
+};
+let selectedTool = toolsMaterial.none;
+tools.forEach((element, i) => {
+  element.addEventListener("click", t, false);
+});
+
+function t(e) {
+  let target = e.target;
+  let child = target.parentElement.firstElementChild;
+  while (child) {
+    console.log(child.classList);
+    console.log(e.target.classList);
+    if (child.classList.contains("selected") && child !== target) {
+      child.classList.toggle("selected");
+    }
+    child = child.nextElementSibling;
+  }
+  target.classList.toggle("selected");
+}
